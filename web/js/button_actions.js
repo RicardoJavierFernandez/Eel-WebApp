ee8ebd137de0d9c2b7db9cd106c53cad5c089b20
RicardoJavierFernandez <rickyjf808@gmail.com>
2019-06-30 09:44:29 -0400

    JS update

--- web/js/button_actions.js
+++ web/js/button_actions.js
@@ -1,15 +1,15 @@
 document.getElementById("reconBtn").addEventListener("click", function createExcel(){
-    window.alert("The Recon Excel file is being generated.");
+    window.alert("Report one is being generated.");
     eel.create_excel_file();
 })
 
 document.getElementById("brokerBtn").addEventListener("click", function createBrokerFile(){
-    window.alert("The Broker Trades Excel file is being generated.");
+    window.alert("Report two is being generated.");
     eel.create_broker_file();
 })
 
 document.getElementById("sepActBtn").addEventListener("click", function createSepActFile(){
-    window.alert("The Separate Account Sector file is being generated.")
+    window.alert("Report three is being generated.")
     eel.create_excel_file();
 })
 
